23ae55c5de89dd22f4bba952f21773d1d21e26fe
Grigoriy Kozheletov <misteryrus@gmail.com>
2022-01-21 11:44:10 +0300

    add scroll navigation

--- scripts.js
+++ scripts.js
@@ -7,6 +7,25 @@ window.addEventListener('load', () => {
   const header = document.querySelector('.header');
   const buttonGoTop = document.querySelector('.js-button-top');
   const navLinks = document.querySelectorAll('.header__nav-link');
+  const scrollButtonArea = document.querySelector('.scroll-area');
+  const scrollElements = document.querySelectorAll('.scroll-element');
+  const scrollButtons = document.querySelectorAll('.scroll-area__button');
+
+  const scrollButtonState = () => {
+    for (const scrollElement of scrollElements) {
+      const elementTopLine = scrollElement.offsetTop - 10;
+      const elementBottomLine = scrollElement.offsetTop + scrollElement.offsetHeight;
+      const currentScroll = window.pageYOffset;
+      if (currentScroll >= elementTopLine && currentScroll < elementBottomLine) {
+        for (const scrollButton of scrollButtons) {
+          scrollButton.classList.remove('scroll-area__button_selected');
+        }
+        scrollButtons[scrollElement.dataset['scrollPosition']].classList.add('scroll-area__button_selected');
+      }
+    }
+  };
+
+  scrollButtonState();
 
   for (let navLink of navLinks) {
     navLink.addEventListener('click', (event) => {
@@ -35,10 +54,29 @@ window.addEventListener('load', () => {
     } else {
       buttonGoTop.classList.remove('js-show-top-button');
     }
+
+    scrollButtonState();
   });
 
   buttonGoTop.addEventListener('click', (event) => {
     event.preventDefault();
     header.scrollIntoView({behavior: 'smooth'});
+  });
+
+  scrollButtonArea.addEventListener('click', (event) => {
+    const currentTarget = event.target;
+
+    if (currentTarget.tagName !== 'BUTTON') {
+      return;
+    }
+
+    const scrollElement = scrollElements[currentTarget.dataset['scrollButton']];
+    scrollElement.scrollIntoView({behavior: 'smooth'});
+
+    for (const scrollButton of scrollButtons) {
+      scrollButton.classList.remove('scroll-area__button_selected');
+    }
+
+    currentTarget.classList.add('scroll-area__button_selected');
   });
 });
